b7ab4749d050302b80d349037838fa271cbbde8a
Tessa <tessa.schijff@hva.nl>
2022-01-11 06:56:39 +0100

    resume kopjes vernederlandst

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -40,7 +40,7 @@ class Resume extends Component {
         ----------------------------------------------- --> */}
         <div className="row education">
            <div className="three columns header-col">
-              <h1><span>Education</span></h1>
+              <h1><span>Opleidingen</span></h1>
            </div>
            <div className="nine columns main-col">
               {education}
@@ -52,7 +52,7 @@ class Resume extends Component {
         ----------------------------------------------- --> */}
         <div className="row work">
            <div className="three columns header-col">
-              <h1><span>Work</span></h1>
+              <h1><span>Werkervaring</span></h1>
            </div>
            <div className="nine columns main-col">
               {work}
@@ -66,11 +66,11 @@ class Resume extends Component {
               <h1><span>Skills</span></h1>
            </div>
            <div className="nine columns main-col">
-              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+              {/* <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
               eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
               voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
               voluptatem sequi nesciunt.
-              </p>
+              </p> */}
                   <div className="bars">
                      <ul className="skills">
                          {skills}
